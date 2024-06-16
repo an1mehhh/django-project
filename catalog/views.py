a07c660040e175fcd31f8381ce085e9eaf1afe17
an1mehhh <an1mehhh@yandex.ru>
2024-06-16 21:10:55 +0300

    first commit

---
+++ catalog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Имя: {name} \nТелефон: {phone} \nСообщение: {message}')
+
+    return render(request, 'catalog/contacts.html')
