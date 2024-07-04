ee38eed6d5d696c6052a75c066eb4123b2656cfb
an1mehhh <an1mehhh@yandex.ru>
2024-07-04 21:32:42 +0300

    add homework 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from catalog.views import index, contact
+from catalog.views import index, contact, product_list, product_detail
 
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contact, name='contacts'),
+    path('product_list/', product_list, name='product_list'),
+    path('product_detail/<int:pk>/', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'category': Category.objects.all()
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+def product_list(request):
+    products = Product.objects.all()  # Получаем все продукты
+    return render(request, 'catalog/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)  # Получаем продукт по его pk
+    return render(request, 'catalog/product_detail.html', {'product': product})
 
 
 def contact(request):
